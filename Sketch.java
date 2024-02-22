95a8673dcd6aae5c69aff284705090b179ddc74a
Emma Chan <emma.chan25@ycdsbk12.ca>
2024-02-22 11:35:05 -0500

    finished

--- Sketch.java
+++ Sketch.java
@@ -11,25 +11,37 @@ public class Sketch extends PApplet {
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(138, 181, 237);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  // verible 
+    int length = 400; 
+    int width = 400;  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // background
+      fill(21, 153, 51);
+      rect(0,(float)(width *0.625),length, width);
+  
+    // roof
+      fill(150,58,38);
+      triangle((float)(length * 0.25), (float)(width * 0.5), (float)(length * 0.5), (float)(width*0.2),(float)(length*0.75),(float)(width*0.5));  
+  
+    //main part of house
+      fill(255,77,207);
+      rect((float)(length*0.25),(float)(width*0.5),(float)(length*0.5),(float)(width*0.5));
+  
+    // window 
+      fill (247, 188, 224);
+      rect((float)(length*0.325), (float)(width*0.6), (float)(length*0.175), (float)(width*0.175));
+  
+    //door 
+      fill (211, 188, 247);
+      rect((float)(length*0.575), (float)(width*0.775), (float)(length*0.15), (float)(width*0.25));
+  
+    //door knob
+      fill(205, 219, 96);
+      ellipse((float)(length*0.625), (float)(width*0.9), (float)(width*0.025), (float)(length*0.025));
   }
   
   // define other methods down here.
